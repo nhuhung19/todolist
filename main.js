ddeaf7e9061c1451486a1686e1745d2b82cdcc0e
nhuhung19 <nhuhung19@gmail.com>
2020-03-10 23:59:44 +0700

    make star piorty

--- main.js
+++ main.js
@@ -1,21 +1,31 @@
 let inputValue = document.getElementById('todoInput')
-    // let btnCheckTask = document.getElementById('show-undone')
+let piortyTask = document.getElementById('piorty')
 let todoList = [];
 
 let see
 let addItem = () => {
     let todoValue = {
         text: inputValue.value,
-        isDone: false
+        isDone: false,
+        piorty: parseInt(piortyTask.value)
     }
     todoList.push(todoValue)
     render(see)
     inputValue.value = ''
+    console.log(todoList)
 }
 
 const removeItem = (index) => {
     todoList.splice(index, 1)
     render(see)
+}
+
+let piortyTodo = (num) => {
+    let piorList = []
+    for (let i = 0; i < num; i++) {
+        piorList.push('<i class="fa fa-star"></i>')
+    }
+    return piorList.join('')
 }
 
 const render = (status) => {
@@ -39,9 +49,9 @@ const render = (status) => {
     let htmlTodoArray = filter.map((item, index) => {
         return `<li><span  style="text-decoration: ${item.isDone ? 'line-through': ''}" onclick="toggleDone(${index})">${item.text}
         <a>${item.isDone ? 'Mark Undone': 'Mark Done'}</a>
-        </span>
+        </span>${piortyTodo(item.piorty)}
         <button onclick="removeItem(${index})">X</button></li>`
-    }).join('')
+    }).sort((a, b) => a.piorty < b.piorty ? 1 : -1).join('')
     document.getElementById('resultArea').innerHTML = htmlTodoArray
     saveTodos()
 
